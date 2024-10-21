e3d69fece9442ea69410f690aa4d2772019266a0
ThanhNT <thanhnt19146391@gmail.com>
2024-10-21 11:20:01 +0700

    Update c

--- contests/educational/170/c.py
+++ contests/educational/170/c.py
@@ -6,39 +6,16 @@ inp = lambda dtype: [dtype(x) for x in input().split()]
 
 for _  in range(int(input())):
     n, k = inp(int)
-    lst_a = inp(int)
-    lst_a.sort()
-    a = array('i', lst_a)
-    # print(n, k, a)
-    b = array('i', [])
-    cnt = array('i', [])
-    break_point = array('i', [])
+    a = inp(int)
+    a.sort()
+    res = 0
     j = 0
     for i in range(n):
-        if len(b) == 0:
-            b.append(a[i])
-            cnt.append(0)
-        if a[i] == b[j]:
-            cnt[j] += 1
-        else:
-            b.append(a[i])
-            cnt.append(1)
-            if a[i] != b[j] + 1:
-                
+        j = max(i, j)
+        while j + 1 < n and a[j + 1] - a[j] <= 1 and a[j + 1] - a[i] < k:
             j += 1
-    # print(b, '\n', cnt)
-
-    m = len(b)
-    sum = array('i', [])
-    for i in range(m):
-        if len(sum) == 0:
-            sum.append(b[i])
-        else:
-            sum.append(sum[i-1] + b[i])
-        
-
-    sum.append(0)
-
-
+        res = max(res, j - i + 1)
+    print(res)
+    
             
 
